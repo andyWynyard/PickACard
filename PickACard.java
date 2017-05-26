acab224b9dc3cee99772870603b612c43e92bbe6
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-05-26 10:53:58 -0600

    not finished

---
+++ PickACard.java
@@ -0,0 +1,15 @@
+class Card {
+	int value;
+	String suit;
+	String name;
+
+	public String toString () {
+		return name + " of " + suit;
+	}
+}
+
+public class PickACard {
+	public static void main(String[] args) {
+		
+	}
+}
